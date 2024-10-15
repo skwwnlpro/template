4128b952c2f6ea3a05abb9982c3e31af3c329da4
skwwnl <skwwnl@gmail.com>
2024-10-15 11:46:06 +0900

    Chore: Template

--- app/entity/event.py
+++
@@ -1,9 +0,0 @@
-from tortoise import fields, models
-
-
-class Event(models.Model):
-    id = fields.IntField(pk=True)
-    name = fields.CharField(max_length=255)
-
-    class Meta:
-        table = "event"
--- app/entity/movie.py
+++
@@ -1,9 +0,0 @@
-from tortoise import fields, models
-
-
-class Movie(models.Model):
-    movie_id = fields.BigIntField(pk=True)
-    movie_name = fields.CharField(max_length=255)
-
-    class Meta:
-        table = "movie"
